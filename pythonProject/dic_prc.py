de8aa0ed8e5ef87d4f6d706c0bd189cae11926ef
HanzalaWahid <136264253+HanzalaWahid@users.noreply.github.com>
2023-10-11 16:52:02 -0700

    Add files via upload

---
+++ pythonProject/dic_prc.py
@@ -0,0 +1,27 @@
+# urdutranslation = {
+#     "Kasy_ho": "I am fine",
+#     "Kiya_kar_rahy_ho": "What are you doing ",
+#     "Kaha_ja_rahay_ho": "where are you going "
+# }
+#
+# a = input("Enter the urdu word: \n")
+# print("Translation of word: ", urdutranslation.get(a))
+#
+# number1 = int(input("Enter any number1: "))
+# number2 = int(input("Enter any number2: "))
+# number3 = int(input("Enter any number3: "))
+# number4 = int(input("Enter any number4: "))
+# number5 = int(input("Enter any number5: "))
+# number6 = int(input("Enter any number6: "))
+# number7 = int(input("Enter any number7: "))
+# number8 = int(input("Enter any number8: "))
+#
+# uniquenumber = number1,number2,number3,number4,number5,number6,number7,number8
+#
+# print("Unique_number",uniquenumber )
+
+s = set()
+s.add(20)
+s.add(20.0)
+s.add("20")
+print(len(s))
