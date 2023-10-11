fc38ba769c8c5dd1936e24ab6ff892aed3dc7f0f
HanzalaWahid <136264253+HanzalaWahid@users.noreply.github.com>
2023-10-11 16:54:13 -0700

    Add files via upload

---
+++ pythonProject/input.py
@@ -0,0 +1,19 @@
+f1 = input("Enter student number 1 marks: ")
+f2 = input("Enter student number 2 marks: ")
+f3 = input("Enter student number 3 marks: ")
+f4 = input("Enter student number 4 marks: ")
+f5 = input("Enter student number 5 marks: ")
+f6 = input("Enter student number 6 marks: ")
+f7 = input("Enter student number 7 marks: ")
+
+print("Student 1 got: ", f1)
+print("Student 2 got: ", f2)
+print("Student 3 got: ", f3)
+print("Student 4 got: ", f4)
+print("Student 5 got: ", f5)
+print("Student 6 got: ", f6)
+print("Student 7 got: ", f7)
+
+score = [f1, f2, f3, f4, f5, f6, f7]
+score.sort()
+print(score)
