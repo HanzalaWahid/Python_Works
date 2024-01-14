78f27e78995760d09e36cd306878813800688062
HanzalaWahid <136264253+HanzalaWahid@users.noreply.github.com>
2024-01-14 16:17:23 +0500

    Add files via upload

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -1,24 +1,35 @@
-import pandas as pd
+# # class circle (object):
+# #     def __init__(self,radius,color):
+# #         self.radius = radius
+# #         self.color = color
+# # redcircle = circle(100,"orange")
+# #
+# # #
+# # example_module.py
+#
+# def function1():
+#     print("This is function 1")
+#
+# def function2():
+#     print("This is function 2")
+#
+# def function3():
+#     print("This is function 3")
 
-# Create a sample DataFrame
-data = {'Name': ['John', 'Alice', 'Bob'],
-        'Age': [28, 24, 22],
-        'City': ['New York', 'San Francisco', 'Los Angeles']}
 
-df = pd.DataFrame(data)
+# list = [1,2,43,21,223,43]
+#
+# my_list = [i*2 for i in list]
+# print(my_list)
+#
+# square = [x*x for x in range (10) if x %2  == 0]
+# print(square)
+#
+# name = ["Hanzala","Mashood","Talha"]
+# roll_number = [46,45,22]
+#
+# name_roll_number = (list(zip(name,roll_number)))
+#
+# for name , roll_number in name_rollnumber:
+#     print(f"Name: {name}, Roll_number: {roll_number}")
 
-# Set 'Name' as the index
-df.set_index('Name', inplace=True)
-
-# Perform some operations that may change the index
-df['Occupation'] = ['Engineer', 'Doctor', 'Teacher']
-
-# Reset the index
-df_reset = df.reset_index()
-
-# Display the original and reset DataFrames
-print("Original DataFrame:")
-print(df)
-
-print("\nDataFrame after resetting the index:")
-print(df_reset)
