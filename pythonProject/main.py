5d377d1e57d255b89fee06aafba7cb38a55d639a
HanzalaWahid <136264253+HanzalaWahid@users.noreply.github.com>
2024-02-12 23:47:56 +0500

    Add files via upload

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -1,37 +1,24 @@
-from sklearn.ensemble import RandomForestClassifier
+import streamlit as st
+st.set_page_config(page_title="My First Website",page_icon=":tada:",layout="wide")
 
-# Fit a model
-model = RandomForestClassifier()
-model.fit(X, y)
+st.subheader("Hey its Me Hanzala :wave:")
+st.title ("I am a Junior Developer from Pakistan")
+st.write ("A passionate developer eager to leran how to develop and deploy website to keen my knowledge as a developer want to enhance my skills")
+st.write ("[Learn more >] (https://github.com/HanzalaWahid)" )
 
-# Get feature importances
-feature_importances = model.feature_importances_
-from sklearn.feature_selection import RFE
 
-# Create an estimator (e.g., a classifier or regressor)
-estimator = RandomForestClassifier()
+with st.container():
+    st.write
+    left_column,right_column  = st.columns([4,1])  # Create two columns with equal width
 
-# Create the RFE model and select the number of features to keep
-rfe = RFE(estimator, n_features_to_select=5)
-rfe.fit(X, y)
-from sklearn.feature_selection import SelectKBest
-from sklearn.feature_selection import chi2
+with left_column:
+    st.header ('About')                            # Add a title for this section
+    st.markdown('''
+                **About me**
+                
+                I'm Wahid Hanzala and I'm currently pursuing my Bachelor of Science in Artificial Intelligence at Dawood University of Engineering &  Technology (DUET). My I am a junior developer who is passionate about learning Python programming languages and technologies. My main
+                Domain is Data Science  and I'm currently learning Python programming language My main interest lies in Data Visualization and Deep Learning.
+                I'm Hanzala Wahid, a junior developer who is passionate about coding
+                ''')
 
-# Select the top k features
-k = 5
-X_new = SelectKBest(chi2, k=k).fit_transform(X, y)
-import pandas as pd
-
-df = pd.get_dummies(df, columns=['categorical_column'])
-from sklearn.preprocessing import LabelEncoder
-
-le = LabelEncoder()
-df['categorical_column'] = le.fit_transform(df['categorical_column'])
-from sklearn.preprocessing import StandardScaler
-
-scaler = StandardScaler()
-df['numerical_column'] = scaler.fit_transform(df['numerical_column'].values.reshape(-1, 1))
-from sklearn.preprocessing import MinMaxScaler
-
-scaler = MinMaxScaler()
-df['numerical_column'] = scaler.fit_transform(df['numerical_column'].values.reshape(-1, 1))
+                
